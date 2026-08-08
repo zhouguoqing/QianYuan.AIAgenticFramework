105014e2407567fb50e8b68451a166a3b4371500
MWT2000 <34787754@qq.com>
2026-08-08 15:58:18 +0800

    Remove local snapshot artifacts

--- artifacts/snapshots/pre-phase2-multiturn-20260806-113636/src/QianYuan.Desktop/dist/common/localComputer.js
+++
@@ -1,6 +0,0 @@
-export const localComputerChannels = {
-    startCommand: 'workpartner:computer:startCommand',
-    getCommand: 'workpartner:computer:getCommand',
-    listCommands: 'workpartner:computer:listCommands',
-    cancelCommand: 'workpartner:computer:cancelCommand',
-};
